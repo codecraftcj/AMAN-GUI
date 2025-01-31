3ab38e9a20ca439bd03ec870d443d4f98bccfdf4
cj <codecraftcj@gmail.com>
2025-01-31 23:42:58 +0800

    adding todo

--- app/(drawer)/control-room.tsx
+++ app/(drawer)/control-room.tsx
@@ -9,34 +9,18 @@ export default function Index() {
     const isMobile = width <= 768; // Example breakpoint for mobile devices
     const router = useRouter();
 
+    // @To do: Control Room Page Design
     return (
       <SafeAreaProvider>
       <SafeAreaView style={{flex: 1}}>
       <View className={isMobile ? "flex-1 bg-white": "flex-1 bg-[#032D61]"}>
       
       {isMobile ? 
-      <View className=' items-center w-[42%] h-[80%] m-auto bg-white rounded-md'>
-      <Image source={require('@/assets/images/aman-logo.png')} style={styles.image}  className='mt-[10%]'/>
-    
-        <TouchableOpacity className='justify-center items-center  bg-[#032D61] h-[10%] w-[120%] mt-[10%] rounded-3xl' onPress={() => router.push('/signIn')}>
-            <Text className='text-white text-lg'>
-                Drawer Devices
-            </Text>
-        </TouchableOpacity>
-  
-    </View>
+      
       : 
       
-      <View className=' items-center w-[536.76px] h-[708px] m-auto bg-white rounded-md'>
-      <Image source={require('@/assets/images/aman-logo.png')} style={styles.image}  className='mt-[10%]'/>
-    
-        <TouchableOpacity className='justify-center items-center  bg-[#032D61] h-[10%] w-[50%] rounded-lg' onPress={() => router.push('/signIn')}>
-            <Text className='text-white text-lg'>
-                Drawer Devices
-            </Text>
-        </TouchableOpacity>
-  
-    </View>}
+      
+      }
 
         </View>
       </SafeAreaView>
